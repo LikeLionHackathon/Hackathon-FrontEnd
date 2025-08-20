2f431351e2c7040b02a2ee553dbd0529a8f048c8
Juyoung03 <iris031008@naver.com>
2025-08-20 20:43:42 +0900

    feat: 메인 작가 페이지 구현

---
+++ src/components/mainpage/ArtistCard.jsx
@@ -0,0 +1,22 @@
+import profileImg from "../../assets/profile.svg";
+import { AiOutlineMessage } from "react-icons/ai";
+
+const ArtistCard = ({img, name, num, place}) => {
+    
+    return (
+        <div className="flex flex-col w-[135px] h-[209px] rounded-[10px] border border-lightpurple01 justify-center items-center">
+            <img src={img || profileImg} alt="profile" className="w-[76px] h-[76px] mb-[11px]"/>
+            <div className="flex flex-col justify-center items-center mb-[8px]">
+                <p className="font-semibold text-[16px]">{name}</p>
+                <p className="text-[12px] text-darkgrey01">{num}명 모집 중</p>
+                <p className="text-[12px]">{place}</p>
+            </div>
+            <button className="w-[98px] h-[25px] bg-purple_main text-white rounded-[5px] flex flex-row justify-center items-center gap-[8px]">
+                <AiOutlineMessage className="w-[16px]"/>
+                <p className="text-[12px] leading-none">문의하기</p>
+            </button>
+        </div>
+    )
+}
+
+export default ArtistCard;
